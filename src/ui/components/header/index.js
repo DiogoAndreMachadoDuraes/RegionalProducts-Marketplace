31181683293235bb134737694e616a30b7beec64
AndreiaSousa <a37525@alunos.ipb.pt>
2021-11-08 16:26:43 +0000

    change de header (name of products)

--- src/ui/components/header/index.js
+++ src/ui/components/header/index.js
@@ -286,38 +286,51 @@ class Header extends Component {
                                                 })
                                         } */}
 
-                    <Dropdown.Header>Azeites</Dropdown.Header>
+                    <Dropdown.Header>Biscoitos</Dropdown.Header>
                     <Dropdown.Item
                       href="/product/:categoryolive/virgin"
                       eventKey="1"
                     >
-                      Azeite Virgem
+                      Compotas
                     </Dropdown.Item>
                     <Dropdown.Item
                       href="/product/olive/extravirgin"
                       eventKey="2"
                     >
-                      Azeite Extra-Virgem
+                      
                     </Dropdown.Item>
-                    <Dropdown.Item href="/product/olive/flavor" eventKey="3">
-                      Azeite Aromatizado
-                    </Dropdown.Item>
-                    <Dropdown.Divider />
+                
 
-                    <Dropdown.Header>Vinhos</Dropdown.Header>
+                    <Dropdown.Header>Compotas</Dropdown.Header>
                     <Dropdown.Item href="/product/wine/tinto" eventKey="1">
-                      Vinho Tinto
+                      Compotas
                     </Dropdown.Item>
                     <Dropdown.Item href="/product/wine/branco" eventKey="2">
-                      Vinho Branco
+                      Mel
                     </Dropdown.Item>
-                    <Dropdown.Item href="/product/wine/rose" eventKey="3">
-                      Vinho Rosé
+
+                    
+                    <Dropdown.Header>Enchidos e Carne</Dropdown.Header>
+                    <Dropdown.Item href="/product/wine/tinto" eventKey="1">
+                      Compotas
                     </Dropdown.Item>
-                    <Dropdown.Item href="/product/wine/espumante" eventKey="4">
-                      Espumante
+                    <Dropdown.Item href="/product/wine/branco" eventKey="2">
+                      Mel
                     </Dropdown.Item>
-                    <Dropdown.Divider />
+
+                    
+                    <Dropdown.Header>Infusões</Dropdown.Header>
+                    <Dropdown.Item href="/product/wine/tinto" eventKey="1">
+                      Café
+                    </Dropdown.Item>
+                    <Dropdown.Item href="/product/wine/branco" eventKey="2">
+                      Chá
+                    </Dropdown.Item>
+
+                    
+                    <Dropdown.Header>Sementes</Dropdown.Header>
+                    
+                    
                   </NavDropdown>
                   <Nav.Link href="/favorites" style={{ color: "white" }}>
                     Favoritos
@@ -394,70 +407,35 @@ class Header extends Component {
                     id="collasible-nav-dropdown"
                     noCaret
                   >
-                    <Dropdown.Header>Azeites</Dropdown.Header>
-                    <Dropdown.Item href="/product/olive/virgin" eventKey="1">
-                      Azeite Virgem
-                    </Dropdown.Item>
-                    <Dropdown.Item
-                      href="/product/olive/extravirgin"
-                      eventKey="2"
-                    >
-                      Azeite Extra-Virgem
-                    </Dropdown.Item>
-                    <Dropdown.Item href="/product/olive/flavor" eventKey="3">
-                      Azeite Aromatizado
-                    </Dropdown.Item>
+                    <Dropdown.Header>Biscoito</Dropdown.Header>
+                  
                     <Dropdown.Divider />
 
-                    <Dropdown.Header>Vinhos</Dropdown.Header>
+                    <Dropdown.Header>Compotas</Dropdown.Header>
                     <Dropdown.Item href="/product/wine/tinto" eventKey="1">
-                      Vinho Tinto
+                      Compota
                     </Dropdown.Item>
                     <Dropdown.Item href="/product/wine/branco" eventKey="2">
-                      Vinho Branco
-                    </Dropdown.Item>
-                    <Dropdown.Item href="/product/wine/rose" eventKey="3">
-                      Vinho Rosé
-                    </Dropdown.Item>
-                    <Dropdown.Item href="/product/wine/espumante" eventKey="4">
-                      Espumante
+                      Mel
                     </Dropdown.Item>
                     <Dropdown.Divider />
 
-                    <Dropdown.Header>Aguardentes</Dropdown.Header>
-                    <Dropdown.Item href="/product/brandy/arbutus" eventKey="1">
-                      Aguardente de Medronho
-                    </Dropdown.Item>
-                    <Dropdown.Item href="/product/brandy/cereals" eventKey="2">
-                      Aguardente de Cereais
-                    </Dropdown.Item>
-                    <Dropdown.Item href="/product/brandy/woodcane" eventKey="3">
-                      Aguardente de Cana de Madeira
-                    </Dropdown.Item>
-                    <Dropdown.Item href="/product/brandy/pear" eventKey="4">
-                      Aguardente de Pêra
-                    </Dropdown.Item>
+                    <Dropdown.Header>Enchidos e Carne</Dropdown.Header>
                     <Dropdown.Divider />
 
-                    <Dropdown.Header>Licores</Dropdown.Header>
-                    <Dropdown.Item
-                      href="/product/liquor/limoncello"
-                      eventKey="1"
-                    >
-                      Licor de Limoncello
-                    </Dropdown.Item>
+                    <Dropdown.Header>Infusões</Dropdown.Header>
                     <Dropdown.Item href="/product/liquor/coffee" eventKey="2">
-                      Licor de Café
-                    </Dropdown.Item>
-                    <Dropdown.Item href="/product/liquor/almonds" eventKey="3">
-                      Licor de Amêndoas
+                      Café
                     </Dropdown.Item>
                     <Dropdown.Item
                       href="/product/liquor/chocolate"
                       eventKey="4"
                     >
-                      Licor de Chocolate
+                      Chá
                     </Dropdown.Item>
+                    <Dropdown.Divider />
+                    <Dropdown.Header>Sementes</Dropdown.Header>
+              
                   </NavDropdown>
                 </Nav>
               </Navbar.Collapse>
